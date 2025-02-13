130a87bb8c50273e9a72985d6902c72437a06eb2
RKDLDE <kangcanddy@gmail.com>
2025-02-13 21:19:21 +0900

    [comment] 3-6 문제 추가

--- src/main/java/com/study/chap03/Ex6.java
+++ src/main/java/com/study/chap03/Ex6.java
@@ -1,6 +1,6 @@
 package com.study.chap03;
 
-/* num 값보다 크면서도 가장 가까운 10의 배수에서 변수 num의 값을 뺀 나머지를 구하는 코드
+/* 3-6. num 값보다 크면서도 가장 가까운 10의 배수에서 변수 num의 값을 뺀 나머지를 구하는 코드
  * 24의 크면서도 가장 가까운 120의 배수는 30, 19의 경우 20 */
 public class Ex6 {
     public static void main(String[] args) {
