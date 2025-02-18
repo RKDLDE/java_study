4302b78cedfb0e40295c48b0d7ea2ee54993b176
RKDLDE <kangcanddy@gmail.com>
2025-02-18 20:06:20 +0900

    [add] 추가

---
+++ src/main/java/com/study/chap04/Ex13.java
@@ -0,0 +1,23 @@
+package com.study.chap04;
+
+public class Ex13 {
+    public static void main(String[] args) {
+//        String value = "12o34";
+//        char ch = ' ';
+//        boolean isNumber = true;
+//
+//        for(int i = 0; i < value.length(); i++){
+//            if(value.charAt(i)-'0' ){
+//                isNumber = false;
+//                break;
+//            }
+//        }
+//
+//        if (isNumber) {
+//            System.out.println(value + "는 숫자입니다.");
+//        } else {
+//            System.out.println(value + "는 숫자가 아닙니다.");
+//        }
+
+    }
+}
---
+++ src/main/java/com/study/chap05/Ex2.java
@@ -0,0 +1,5 @@
+package com.study.chap05;
+
+/* 5-2. 다음과 같은 배열이 있을 때, arr[3].length의 값은 얼마인가? */
+public class Ex2 {
+}
